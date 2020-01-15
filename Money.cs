cc6fbc2f80c4a7247e1aa95aeb32e912e254025c
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-15 15:25:54 -0500

    Implement single-argument constructor

--- Money.cs
+++ Money.cs
@@ -16,5 +16,11 @@ namespace money_class
             dollars = 46;
             cents = 75;
         }
+        // accept single argument, per spec
+        public Money (double dollarsAndCents)
+        {
+            dollars = Math.Floor(dollarsAndCents);
+            cents = (dollarsAndCents - Math.Floor(dollarsAndCents)) * 100;
+        }
     }
 }
