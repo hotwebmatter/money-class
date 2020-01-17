0a06232fe1281adc7539eec9b09e2e11f78a398b
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-16 19:57:54 -0500

    Test MakeChange() method with all constructors

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace money_class
             Money bMoney = new Money(3.27);
             Money cMoney = new Money(17, 48);
             Console.WriteLine("{0}: {1}\n", "aMoney", aMoney.ToString());
+            Console.WriteLine(aMoney.MakeChange());
             Console.WriteLine("{0}: {1}\n", "bMoney", bMoney.ToString());
+            Console.WriteLine(bMoney.MakeChange());
             Console.WriteLine("{0}: {1}\n", "cMoney", cMoney.ToString());
+            Console.WriteLine(cMoney.MakeChange());
         }
     }
 }
