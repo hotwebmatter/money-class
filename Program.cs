c18411f92f6f7463fbab31d7689e85573a095cef
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-16 19:15:42 -0500

    Test dual-argument constructor

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace money_class
         {
             Money aMoney = new Money();
             Money bMoney = new Money(3.27);
+            Money cMoney = new Money(17, 48);
             Console.WriteLine("Hello World!");
         }
     }
